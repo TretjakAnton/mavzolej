97fad6600ba6570daa1481f0bbfa825833369d2b
TretjakAnton <tretjak.anton@gmail.com>
2016-11-01 23:32:48 +0200

    serverReq add requests

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var path = require('path'),
   bodyParser = require('body-parser'),
   app = express(),
   mysql   = require('mysql'),
-  router = express.Router();
+  router = express.Router(),
+  formidable = require('formidable'),
+  fs = require('fs');
 
 var pool = mysql.createPool({
   host     : 'localhost',
@@ -27,28 +29,151 @@ app.use(bodyParser.urlencoded({
 
 //api route config
 router.route('/login')
-.post(function (req, res) {
-  debugger;
-  if (req.body.login == 'Username' && req.body.password == 'Password') {
-    res.json({Auth: 'Logged'});
-  } else {
-    res.json({Auth: 'Denied'});
-  }
-});
+  .post(function (req, res) {
+    debugger;
+    if (req.body.login == 'Username' && req.body.password == 'Password') {
+      res.json({Auth: 'Logged'});
+    } else {
+      res.json({Auth: 'Denied'});
+    }
+  });
 
 router.route('/allTypes')
-    .post(function(req, res){
-      pool.getConnection(function(err, connection) {
+    .post(function (req, res) {
+      pool.getConnection(function (err, connection) {
         var queryTo = 'SELECT name_type from type';
         connection.query(queryTo, function (err, rows) {
-          if(!err && rows.length > 0) {
+          if (!err && rows.length > 0) {
             res.json(rows);
           } else {
-            res.json({error: 'Sorry we have error on the server. Please try again later'});
+            res.json({error: err.message});
           }
           connection.release();
         });
       });
+    })
+    .put(function (req, res) {
+        pool.getConnection(function (err, connection) {
+            var queryTo = 'INSERT INTO type (name_type) VALUES ("'+req.body.name_type+'")';
+            connection.query(queryTo, function (err) {
+                if (!err) {
+                    res.json({success: 'successful added'});
+                } else {
+                    res.json({error: err.message});
+                }
+                connection.release();
+            });
+        });
+    });
+
+router.route('/pam')
+    .post(function (req, res) {
+      pool.getConnection(function (err, connection) {
+        var queryTo = 'INSERT INTO pam (id_fake, id_pam, id_type, opis, price, id_size) VALUES ("","'+req.body.id_pam+'","'+req.body.id_type+'","'+req.body.opis+'","'+req.body.id_price+'","'+req.body.id_size+'",)';
+        connection.query(queryTo, function (err) {
+          if (!err) {
+            res.json({success: 'insert success'});
+          } else {
+            res.json({error: err.message});
+          }
+          connection.release();
+        });
+      });
+    })
+    .put(function (req, res) {
+      pool.getConnection(function (err, connection) {
+        var queryTo = 'UPDATE pam SET id_pam="'+req.body.id_pam+'", id_type="'+req.body.id_type+'", opis="'+req.body.opis+'", price="'+req.body.id_price+'", id_size="'+req.body.id_size+'" WHERE id_fake="'+req.body.id_fake+'"';
+        connection.query(queryTo, function (err) {
+          if (!err) {
+            res.json({success: 'update success'});
+          } else {
+            res.json({error: err.message});
+          }
+          connection.release();
+        });
+      });
+    })
+    .delete(function (req, res) {
+        pool.getConnection(function (err, connection) {
+            var queryTo = 'SELECT COUNT(*) FROM images WHERE id_zapis="'+req.body.id_zapis+'"';
+            connection.query(queryTo, function (err, rows) {
+                if (!err && rows.length > 1) {
+                    pool.getConnection(function (err, connection) {
+                        var queryTo = 'DELETE FROM images WHERE id_image="'+req.body.id_image+'"';
+                        connection.query(queryTo, function (err) {
+                            if (!err) {
+                                res.json({success: 'success deleted image'});
+                            } else {
+                                res.json({error: err.message});
+                            }
+                            connection.release();
+                        });
+                    });
+                }
+                if(!err && rows.length == 1){
+                    pool.getConnection(function (err, connection) {
+                        var queryTo = 'DELETE FROM pam WHERE id_fake="'+req.body.id_fake+'"';
+                        connection.query(queryTo, function (err) {
+                            if (!err) {
+                                res.json({success: 'success deleted pam'});
+                            } else {
+                                res.json({error: err.message});
+                            }
+                            connection.release();
+                        });
+                    });
+                }
+                if(err) {
+                    res.json({error: err.message});
+                }
+                connection.release();
+            });
+        });
+    })
+    .get(function (req, res) {
+        pool.getConnection(function (err, connection) {
+            var queryTo = 'SELECT * from pam WHERE id_fake="'+req.body.id_fake+'"';
+            connection.query(queryTo, function (err, rows) {
+                if (!err && rows.length > 0) {
+                    res.json(rows);
+                } else {
+                    res.json({error: err.message});
+                }
+                connection.release();
+            });
+        });
+    });
+
+router.route('/image')
+    .post('/upload', function (req, res) {
+
+        // create an incoming form object
+        var form = new formidable.IncomingForm();
+
+        // specify that we want to allow the user to upload multiple files in a single request
+        form.multiples = true;
+
+        // store all uploads in the /uploads directory
+        form.uploadDir = path.join(__dirname, '/uploads');
+
+        // every time a file has been uploaded successfully,
+        // rename it to it's orignal name
+        form.on('file', function (field, file) {
+            fs.rename(file.path, path.join(form.uploadDir, file.name));
+        });
+
+        // log any errors that occur
+        form.on('error', function (err) {
+            console.log('An error has occured: \n' + err);
+        });
+
+        // once all the files have been uploaded, send a response to the client
+        form.on('end', function () {
+            res.end('success');
+        });
+
+        // parse the incoming request containing the form data
+        form.parse(req);
     });
 
 app.use('/api', router);
--- src/app/api/serverRequests.js
+++ src/app/api/serverRequests.js
@@ -1,7 +1,8 @@
 import 'fetch-polyfill';
+const url = "http://localhost:3000";
 
 export function login(password, login){
-  return fetch("http://localhost:3000/api/login",{
+  return fetch(url + "/api/login",{
     method: 'POST',
     headers: {
       'Accept': 'application/json',
@@ -9,13 +10,94 @@ export function login(password, login){
     },
     body: JSON.stringify({
       password: password,
-      login: login,
+      login: login
     })
   })
   .then(response => response.json())
 }
 
 export function getTypes(){
-  return fetch("http://localhost:3000/api/allTypes",{method: 'post'})
+  return fetch(url + "/api/allTypes",{method: 'post'})
       .then(response => response.json())
+}
+
+export function addType(name){
+  return fetch(url + "/api/allTypes",{
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name_type: name
+    })
+  })
+  .then(response => response.json())
+}
+
+export function addPam(id_pam, id_type, opis, id_price, id_size){
+  return fetch(url + "/api/pam",{
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id_pam: id_pam,
+      id_type: id_type,
+      opis: opis,
+      id_price: id_price,
+      id_size: id_size
+    })
+  })
+  .then(response => response.json())
+}
+
+export function updatePam(id_pam, id_type, opis, id_price, id_size, id_fake){
+  return fetch(url + "/api/pam",{
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id_pam: id_pam,
+      id_type: id_type,
+      opis: opis,
+      id_price: id_price,
+      id_size: id_size,
+      id_fake: id_fake
+    })
+  })
+  .then(response => response.json())
+}
+
+export function deletePam(id_zapis, id_image, id_fake){
+  return fetch(url + "/api/pam",{
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id_zapis: id_zapis,
+      id_image: id_image,
+      id_fake: id_fake
+    })
+  })
+  .then(response => response.json())
+}
+
+export function getPam(id_fake){
+  return fetch(url + "/api/pam",{
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id_fake: id_fake
+    })
+  })
+  .then(response => response.json())
 }
