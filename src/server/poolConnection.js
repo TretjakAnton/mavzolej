0d9131026ca1543f6098f9dd80d6153e3e9e5d76
Anton Tretjak <tretjak.anton@gmail.com>
2017-06-21 16:46:20 +0300

    change host mysql

--- src/server/poolConnection.js
+++ src/server/poolConnection.js
@@ -1,7 +1,7 @@
 var mysql   = require('mysql');
 
 exports.pool = mysql.createPool({
-  host     : 'http://mavzolejmaster.com',
+  host     : '127.0.0.1',
   port     : '3306',
   user     : 'root',
   password : '3263206toxa',
