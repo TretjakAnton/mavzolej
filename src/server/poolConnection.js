27019580456486b05fc78c977714d7149bc7ce71
Anton Tretjak <tretjak.anton@gmail.com>
2017-06-22 11:03:24 +0300

    puul user change

--- src/server/poolConnection.js
+++ src/server/poolConnection.js
@@ -3,7 +3,7 @@ var mysql   = require('mysql');
 exports.pool = mysql.createPool({
   host     : '104.236.196.140',
   port     : '3306',
-  user     : 'root',
+  user     : 'oleg',
   password : '3263206toxa',
   database : 'newpam'
 });
