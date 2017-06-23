da5273cd732141590b9bcaeae1b6a324f62e608c
Anton Tretjak <tretjak.anton@gmail.com>
2017-06-23 15:05:22 +0300

    commit to re-run dokku

--- src/app/Constants/index.js
+++ src/app/Constants/index.js
@@ -1,4 +1,4 @@
-//export const url = "http://localhost:3000";
+// export const url = "http://localhost:3000";
 export const url = "";
 export const ALL_TYPES = 'ALL_TYPES';
 export const ALL_SIZES = 'ALL_SIZES';
