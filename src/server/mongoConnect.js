0ee98b7e3d335c3ebf54923e4201ec2fcf6dd60d
Anton Tretjak <tretjak.anton@gmail.com>
2018-01-14 18:29:00 +0100

    change dbconnection

--- src/server/mongoConnect.js
+++ src/server/mongoConnect.js
@@ -1,7 +1,8 @@
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
-var url = 'mongodb://mavzolej:13a357b25a39c35f673694f414c6254d@dokku-mongo-mavzolej:27017/mavzolej';
+var url = process.env.MONGO_URL;
+// 'mongodb://mavzolej:13a357b25a39c35f673694f414c6254d@dokku-mongo-mavzolej:27017/mavzolej';
 //'mongodb://localhost:27017/myproject';
 
 exports.connect = (app) => {
