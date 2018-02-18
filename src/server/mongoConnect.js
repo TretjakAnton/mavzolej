0dad445db56c17b0f99b101cf380cc1c6b117779
Anton Tretjak <tretjak.anton@gmail.com>
2018-02-18 16:13:23 +0100

    connection change

--- src/server/mongoConnect.js
+++ src/server/mongoConnect.js
@@ -1,8 +1,8 @@
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
-var url = 'mongodb://localhost:27017/myproject';
-//process.env.MONGO_URL;
+var url = process.env.MONGO_URL;
+//'mongodb://localhost:27017/myproject';
 
 exports.connect = (app) => {
   MongoClient.connect(url, function(err, db) {
