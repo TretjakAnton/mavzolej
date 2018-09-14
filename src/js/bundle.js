25f812dd7a3373643375478d7de44002cf761ab5
Anton Tretjak <tretjak.anton@gmail.com>
2018-09-14 17:15:43 +0200

    about us email fix

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -59112,7 +59112,7 @@ var AboutUs = function (_React$Component) {
               _react2.default.createElement('br', null),
               '\u043F\u0440\u0438 \u0434\u0432\u0438\u0436\u0435\u043D\u0438\u0438 \u0441\u043E \u0441\u0442\u043E\u0440\u043E\u043D\u044B \u0421\u043B\u0430\u0442\u0438\u043D\u043E - \u043F\u043E \u0443\u043B\u0438\u0446\u0435 \u041F\u0435\u0442\u0440\u043E\u0432\u0441\u043A\u043E\u0433\u043E \u043F\u043E\u0440\u044F\u0434\u043A\u0430 500 \u043C\u0435\u0442\u0440\u043E\u0432 \u043E\u0442 \u0441\u0442\u0430\u0434\u0438\u043E\u043D\u0430 \u043F\u043E\u0432\u043E\u0440\u043E\u0442 \u043D\u0430\u043B\u0435\u0432\u043E.',
               _react2.default.createElement('br', null),
-              'E-mail: mavzolej-master@yandex.ru',
+              'E-mail: mavzolej-master@gmail.com',
               _react2.default.createElement('br', null)
             )
           )
