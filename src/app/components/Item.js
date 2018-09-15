d71380635f5d504b0689da96e5817111c127626f
Anton Tretjak <tretjak.anton@gmail.com>
2018-09-15 18:36:20 +0200

    added slider

--- src/app/components/Item.js
+++ src/app/components/Item.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Table, Glyphicon, Button } from 'react-bootstrap';
 import { addBodyClass } from '../helpers';
 import Slider from './Slider';
+import SimpleModalSlideshow from 'react-simple-modal-slideshow';
 
 const styles = {
   byButton: {
@@ -21,12 +22,12 @@ class Item extends React.Component{
     this.state = {
       error: '',
       images: [],
+      sliderImages: [],
       info: this.props.info,
       status: false,
-      scrollPos: 0,
       sliderInfo: {
-        imageToShow: null,
         status: false,
+        currentSlide: 0
       }
     };
   };
@@ -34,26 +35,23 @@ class Item extends React.Component{
   componentWillMount = () => {
     const { info } = this.state;
     let newImages = [];
+    let newSliderImages = [];
     info.images.map((image) => {
       newImages.push(`../../../media/images${info.folder}/${image}`);
+      newSliderImages.push({
+        media: (
+          <img src={`../../../media/images${info.folder}/${image}`} />
+        ),
+      });
     });
-    this.setState({ images: newImages });
+    this.setState({
+      images: newImages,
+      sliderImages: newSliderImages
+    });
   }
 
   showHideImages = () => {
     this.setState({ status: !this.state.status })
-  };
-
-  runSlider = (image) => {
-    const scrollPos = document.body.scrollTop || document.documentElement.scrollTop;
-    this.setState({
-      sliderInfo: {
-        imageToShow: image,
-        status: true,
-        scrollPos: scrollPos,
-      }
-    });
-    addBodyClass(true);
   };
 
   openForm = () => {
@@ -63,24 +61,41 @@ class Item extends React.Component{
   closeSlider = () => {
     this.setState({
       sliderInfo: {
-        imageToShow: null,
         status: false,
-        scrollPos: 0,
+        currentSlide: 0,
       }
     });
-    addBodyClass(false, this.state.scrollPos);
   };
 
+  handleNextPrev = (index) => {
+    this.setState({
+      sliderInfo: {
+        status: true,
+        currentSlide: index,
+      }
+    });
+  };
+
+  openSlideshow = (index) => {
+    this.setState({
+      sliderInfo: {
+        status: true,
+        currentSlide: index,
+      }
+    });
+  }
+
   render() {
-    const { info, images, status } = this.state;
+    const { info, images, status, sliderImages } = this.state;
     const imgExist = images && images.length > 1;
     const sliderStatus = this.state.sliderInfo.status;
+    const currentSlide = this.state.sliderInfo.currentSlide;
     const imageToShow = this.state.sliderInfo.imageToShow;
     return (
       <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3">
         <div className="item">
         <div className="mainImg">
-          <img src={images[0]} className="img-responsive" onClick={() => this.runSlider(images[0])}/>
+          <img src={images[0]} className="img-responsive" onClick={() => this.openSlideshow(0)}/>
         </div>
           {imgExist &&
             <Button
@@ -99,7 +114,7 @@ class Item extends React.Component{
           <div className="images">
             {
               this.state.images.map((val, key) => {
-                return <img key={key} src={val} className="img-responsive" onClick={() => this.runSlider(val)} />;
+                return <img key={key} src={val} className="img-responsive" onClick={() => this.openSlideshow(key)} />;
               })
             }
           </div>
@@ -125,7 +140,16 @@ class Item extends React.Component{
             <Glyphicon glyph="shopping-cart" />
           </Button>
         </div>
-        {sliderStatus && <Slider images={images} currImg={imageToShow} onClose={this.closeSlider}/> }
+        <SimpleModalSlideshow 
+          slides={sliderImages}
+          currentSlide={currentSlide}
+          open={sliderStatus}
+          onClose={this.closeSlider}
+          onNext={this.handleNextPrev}
+          onPrev={this.handleNextPrev}
+          classNamePrefix="modal-slideshow"
+        />
+        {/* sliderStatus && <Slider images={images} currImg={imageToShow} onClose={this.closeSlider}/> */}
         </div>
       </div>
     )
